ca9d1bf92c5781bcaf93ab2cf1bb47b85881996e
HoYeon Jang <cliched.0-hoedown@icloud.com>
2024-09-27 16:17:55 +0900

    add teleport analysis function

--- analysis/figS13/plot_teleport.m
+++ analysis/figS13/plot_teleport.m
@@ -1,6 +1,61 @@
-function [] = plot_teleport(DA, eventlog_post, DA, testidx)
-% find trial start
-n_trial = find(eventlog_post(:, 1) == 1);
-% 
+function [] = plot_teleport(DA, trial_on, reward_on, teleport_on, grp)
+    DA_in_trial = nan([size(trial_on, 1), 9]);
+    for i = 1:size(trial_on, 1)
+        trial_length = reward_on(i) - trial_on(i) + 1;
+        DA_in_trial(i, 1:trial_length) = DA(trial_on(i):reward_on(i));
+    end
+    data = table(DA_in_trial(2001:end, :), grp(2001:end, :), 'VariableNames', {'DA', 'grp'});
+    data = sortrows(data, 'grp');
+    unq_grp = unique(data.grp);
+    mu_DA = nan([numel(unq_grp), 9]);
+    sigma_DA = nan([numel(unq_grp), 9]);
+    for i = 1:size(mu_DA, 1)
+        mu_DA(i, :) = mean(data.DA(data.grp == unq_grp(i), :));
+        sigma_DA(i, :) = std(data.DA(data.grp == unq_grp(i), :));
+    end
+    mu_DA_alter = mean(data.DA(1:find(trial_on < teleport_on(1), 1, 'last')-2000, :));
+    sigma_DA_alter = std(data.DA(1:find(trial_on < teleport_on(1), 1, 'last')-2000, :));
 
+    figure;
+    imagesc(data.DA);
+    xlabel('Cue');
+    ylabel('Trial');
+    colorbar;
+    title('Dopamine activity predicted by the ANCCR model');
+
+    figure;
+    errorbar(1:9, mu_DA_alter, sigma_DA_alter);
+    hold on;
+    errorbar(1:9, mu_DA(2, :), sigma_DA(2, :));
+    errorbar(1:9, mu_DA(3, :), sigma_DA(3, :));
+    errorbar(1:9, mu_DA(4, :), sigma_DA(4, :));
+    hold off;
+    xlabel('Cue');
+    ylabel('Dopamine activity');
+    legend({'Standard', 'Teleport 1', 'Teleport 2', 'Teleport 3'});
+    title('Average dopamine activity predicted by the ANCCR model');
+
+    figure;
+    errorbar(1:9, mu_DA_alter, sigma_DA_alter, '-o');
+    hold on;
+    plot(1:9, data.DA(find(data.grp == 2, 1), :));
+    plot(1:9, data.DA(find(data.grp == 4, 1), :));
+    plot(1:9, data.DA(find(data.grp == 6, 1), :));
+    hold off;
+    xlabel('Cue');
+    ylabel('Dopamine activity');
+    legend({'Standard', 'Teleport 1', 'Teleport 2', 'Teleport 3'});
+    title(sprintf('First teleport dopamine activity predicted by the ANCCR model\n(aligned by trial on)'));
+
+    figure;
+    errorbar(1:9, mu_DA_alter, sigma_DA_alter, '-o');
+    hold on;
+    plot(2:9, data.DA(find(data.grp == 2, 1), 1:8));
+    plot(2:9, data.DA(find(data.grp == 4, 1), 1:8));
+    plot(2:9, data.DA(find(data.grp == 6, 1), 1:8));
+    hold off;
+    xlabel('Cue');
+    ylabel('Dopamine activity');
+    legend({'Standard', 'Teleport 1', 'Teleport 2', 'Teleport 3'});
+    title(sprintf('First teleport dopamine activity predicted by the ANCCR model\n(aligned by reward on)'));
 end
--- analysis/figS13/util_process_data.m
+++ analysis/figS13/util_process_data.m
@@ -1,16 +1,10 @@
-function [log, event, grp] = util_process_data(eventlog_pre, eventlog_post)
-log = [eventlog_pre; eventlog_post];
-trial_on = find(log(:, 1) == 1);
-reward_on = find(log(:, 3) == 1);
-teleport_on = find(diff(log(:, 1)) == 2);
-grp = ones(size(trial_on));
-for i = 1:size(teleport_log, 1)
-    grp(find(trial_on <= teleport_on(i), 1, 'last')) = log(teleport_on(i), 1)+1;
-end
-
-event = table( ...
-    'trial_on', trial_on, ...
-    'reward_on', reward_on, ...
-    'teleport_on', teleport_on ...
-    );
+function [trial_on, reward_on, teleport_on, grp] = util_process_data(eventlog_pre, eventlog_post)
+    log = [eventlog_pre; eventlog_post];
+    trial_on = find(log(:, 1) == 1);
+    reward_on = find(log(:, 3) == 1);
+    teleport_on = find(diff(log(:, 1)) == 2);
+    grp = ones(size(trial_on));
+    for i = 1:size(teleport_on, 1)
+        grp(find(trial_on <= teleport_on(i), 1, 'last')) = log(teleport_on(i), 1)+1;
+    end
 end
